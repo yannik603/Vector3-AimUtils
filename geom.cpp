8f36bc56737fdd3e0fc5e463687f0de42a75de28
Yannik Castro <yannik603@gmail.com>
2025-01-28 19:02:36 -0700

    Optional Parameters

--- geom.cpp
+++ geom.cpp
@@ -1,6 +1,7 @@
 #include "geom.h"
 #include <cmath>
 #include <string>
+#include <format>
 
 // Calculates clip coordinates based on OpenGL's model-view-projection matrix and converts them to screen space
 bool OpenGLWorldToScreen(vec3 pos, vec3& screen, const float matrix[16], int windowWidth, int windowHeight)
@@ -59,26 +60,31 @@ Vec3 OpenGLWorldToScreen(Vec3& pos, const float matrix[16], int windowWidth, int
 
 Vec3 DirectXWorldToScreen(Vec3& pos, const float matrix[16], int windowWidth, int windowHeight)
 {
-	vec3 screen;
-	if (DirectXWorldToScreen(pos, screen, matrix, windowWidth, windowHeight))
-		return screen;
-	return vec3(0.0f, 0.0f, 0.0f); // Return a default vector if not visible
+    vec3 screen;
+    if (DirectXWorldToScreen(pos, screen, matrix, windowWidth, windowHeight))
+        return screen;
+    return vec3(0.0f, 0.0f, 0.0f); // Return a default vector if not visible
 }
 
-// Gets the angle of a ray from origin to target
-Vec3 CalcAngle(Vec3& origin, Vec3& target)
+// Gets the angle of a ray from origin to target // InvertPitch means looking up leads to negative pitch
+Vec3 CalcAngle(const Vec3& origin, const Vec3& target, bool invertYaw, bool invertPitch)
 {
     Vec3 results{ 0.0f, 0.0f, 0.0f };
-    results.x = RadiansToDegrees(-atan2f(target.x - origin.x, target.y - origin.y));
-    if (results.x <= 0.0f) {
-        results.x += 360.0f;
-    }
+    Vec3 delta = target - origin; // Get the difference between the two vectors
 
-    results.y = RadiansToDegrees(asinf((target.z - origin.z) / origin.Distance(target)));
+    float distance = origin.Distance(target);
+    if (distance == 0.0f) // Prevent division by zero
+        return results;
+
+    float yawSign = invertYaw ? -1.0f : 1.0f;
+    float pitchSign = invertPitch ? -1.0f : 1.0f;
+
+    results.x = yawSign * RadiansToDegrees(atan2f(delta.y, delta.x));
+    results.y = pitchSign * RadiansToDegrees(asinf((delta.z) / distance));
     return results;
 }
 
-Vec3 DegreesToRadians(Vec3& vec)
+Vec3 DegreesToRadians(const Vec3& vec)
 {
     return Vec3{ DegreesToRadians(vec.x), DegreesToRadians(vec.y), DegreesToRadians(vec.z) };
 }
@@ -98,19 +104,22 @@ float RadiansToDegrees(float num)
     return num / PI * 180.0f;
 }
 
-void Vector3::NormalizeAngle()
+void Vector3::NormalizeAngle(float yawMin, float yawMax, float pitchMin, float pitchMax)
 {
-    if (x > 360)
-        x -= 360;
-    if (x < 0)
-        x += 360;
-    if (y > 90)
-        y -= 90;
-    if (y < -90)
-        y += 90;
+    // Wrap yaw (x) to [yawMin, yawMax] (e.g., [-180, 180])
+    while (x > yawMax)
+        x -= (yawMax - yawMin);
+    while (x < yawMin)
+        x += (yawMax - yawMin);
+
+    // Wrap pitch (y) to [pitchMin, pitchMax] (e.g., [-89, 89])
+    while (y > pitchMax)
+        y -= (pitchMax - pitchMin);
+    while (y < pitchMin)
+        y += (pitchMax - pitchMin);
 }
 
 std::string Vector3::ToString()
 {
-    return std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z);
+    return std::format("{} {} {}", x, y, z);
 }
